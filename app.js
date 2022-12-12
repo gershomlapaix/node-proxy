700f1d200dc596588d2131d60db9db90a69db878
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-13 00:55:05 +0200

    feat: apply rate limit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config({path:'.env'})
+const rateLimit = require('express-rate-limit')
+require('dotenv').config({
+    path: '.env'
+})
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Rate limit
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 5
+})
+
+app.use(rateLimit)
+app.set('trust-proxy', 1)
 
 // ROUTES
 app.use('/api', require('./routes/index'))
